30a9f3a42d5c9ba3b01ed5cdd5fe3f35376c12dd
Poljak Marijan <marijan.poljak@croatiaairlines.hr>
2022-07-18 14:46:18 +0200

    Updates

---
+++ precache-manifest.9e589e0f7e9cf3da687cdbc77b346d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f1363ff2a8861554111",
+    "url": "/react-login/static/css/main.468e4690.chunk.css"
+  },
+  {
+    "revision": "2f1363ff2a8861554111",
+    "url": "/react-login/static/js/main.2f1363ff.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/react-login/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "652b85b9fd03680e12da",
+    "url": "/react-login/static/js/2.652b85b9.chunk.js"
+  },
+  {
+    "revision": "c0f10b70e2ae7528ae35",
+    "url": "/react-login/static/js/runtime~main.c0f10b70.js"
+  },
+  {
+    "revision": "87ebb9c0c1131e018d1e1ed066788278",
+    "url": "/react-login/index.html"
+  }
+];
